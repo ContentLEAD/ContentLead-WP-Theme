a384790cda4a25d602877993696200f84b5e552d
alimfree <alimf@bc.edu>
2014-11-05 11:22:50 -0500

    Allowing comments to be disabled via Settings/Discussion
    No longer hard coding comments template first checking if comments are enabled.

--- single.php
+++ single.php
@@ -35,7 +35,12 @@
 
 			</div>
 		
-			<?php comments_template(); ?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				?>
 
 		</article>
 
