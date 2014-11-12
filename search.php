96678bfbd9e143ff3f2a598a39195e556197ee38
mndewitt <mdewitt07@gmail.com>
2014-11-12 10:04:13 -0500

    fix broken search.php and improve formatting

--- search.php
+++ search.php
@@ -1,37 +1,37 @@
 <?php get_header(); ?>
+<section id="news">
 
 	<?php if (have_posts()) : ?>
-
-		<h2>Search Results</h2>
-
-		<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
+		<div id="primary">
+		<h1>Search Results</h1>
 
 		<?php while (have_posts()) : the_post(); ?>
 
-			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-				<h2><?php the_title(); ?></h2>
-
-				<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
-
-				<div class="entry">
-
+			<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
+				<header class="entry-header">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php echo get_the_post_thumbnail(get_the_ID(), 'latest-news-thumb'); ?></a>
+					<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<time datetime="<?php echo date(DATE_W3C); ?>" pubdate ><?php the_time('F jS, Y') ?></time>
+					<span class="byline author vcard">
+						<span>by</span> <span class="fn"><?php the_author() ?></span>
+					</span>
+				</header>		
+				<div class="entry-content">
 					<?php the_excerpt(); ?>
-
 				</div>
-
 			</article>
 
 		<?php endwhile; ?>
-
-		<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
 
 	<?php else : ?>
 
 		<h2>No posts found.</h2>
 
 	<?php endif; ?>
+</div>
 
 <?php get_sidebar(); ?>
+
+</section>
 
 <?php get_footer(); ?>
